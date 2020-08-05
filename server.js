c0a52a33816d437832450b444c44dc3764526ad6
FehSchiavon <felipschiavon@gmail.com>
2020-08-05 06:38:01 -0300

    Configurate Server | Open

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const server = express()
+
+server.get('/', function (req, res) {
+    return res.send("Open")
+})
+
+server.listen(7000, function() {
+    console.log('Server ON | Port 7000')
+})
