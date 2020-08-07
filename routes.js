f3456009811cbb2ec831d5d704e439986ab4fe58
FehSchiavon <felipschiavon@gmail.com>
2020-08-07 06:46:10 -0300

    routes okay | next controllers

--- routes.js
+++ routes.js
@@ -9,7 +9,14 @@ routes.get('/home', home.index)
 
 // Recipes
 // CRUD
+routes.get('/admin/recipes', recipes.index)
+routes.get('/admin/recipes/create', recipes.create)
+routes.get('/admin/recipes/:id', recipes.show)
+routes.get('admin/recipes/edit', recipes.edit)
 
+routes.post('/admin/recipes', recipes.post)
+routes.put('/admin/recipes', recipes.put)
+routes.delete('/admin/recipes', recipes.delete)
 
 
 module.exports = routes
