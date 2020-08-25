8138b0f8311c4fac5bc1bf90bc265aef52f72f40
FehSchiavon <felipschiavon@gmail.com>
2020-08-25 20:42:36 -0300

    script extern addIngredients

---
+++ public/admin/scripts/addIngredients.js
@@ -0,0 +1,7 @@
+// Function ADD newInput
+function addIngredient() {
+    const ingredients = document.querySelector(".inputIngredients")
+    console.log("OK")
+}
+
+document.querySelector("#addIngredients").addEventListener("click", addIngredient)
