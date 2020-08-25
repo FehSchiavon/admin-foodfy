1a7523e9f5fd62f55212073b00a89f54a9496016
FehSchiavon <felipschiavon@gmail.com>
2020-08-25 20:52:44 -0300

    create function newField

--- public/admin/scripts/addIngredients.js
+++ public/admin/scripts/addIngredients.js
@@ -1,6 +1,9 @@
 // Function ADD newInput
 function addIngredient() {
-    const ingredients = document.querySelector(".inputIngredients")
+    const ingredients = document.querySelector(".ingredientsEdit")
+    const fieldContainer = document.querySelector(".inputIngredients")
+
+    const newField = fieldContainer[fieldContainer.lenght - 1].cloneNode(true)
     console.log("OK")
 }
 
