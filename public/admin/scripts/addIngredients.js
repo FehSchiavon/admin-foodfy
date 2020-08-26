d9470465e3387fd0064698e52183f962cde7da3c
FehSchiavon <felipschiavon@gmail.com>
2020-08-26 06:46:46 -0300

    Clone, but need it is empty

--- public/admin/scripts/addIngredients.js
+++ public/admin/scripts/addIngredients.js
@@ -12,14 +12,14 @@ function addIngredient() {
 
     // const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
     const lengthField = fieldContainerAll.length - 1
-    const cloneField = lengthField.cloneNode(true)
+    // const cloneField = lengthField.cloneNode(true)
     console.log(lengthField)
 
     // if (newField.children[0].value == "") return false
 
     // newField.children[0].value = ""
     ingredients.appendChild(newField)
-    ingredients.appendChild(cloneField)
+    // ingredients.appendChild(cloneField)
 
     console.log("OK")
 }
