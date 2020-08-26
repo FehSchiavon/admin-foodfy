544cd610df294b577ef5ce39a1a3b4a9953027c0
FehSchiavon <felipschiavon@gmail.com>
2020-08-26 06:44:44 -0300

    learn to use .cloneNode()

--- public/admin/scripts/addIngredients.js
+++ public/admin/scripts/addIngredients.js
@@ -3,18 +3,23 @@ function addIngredient() {
     // const fieldContainer = document.createElement("input")
     // document.body.appendChild(fieldContainer)
 
-    const ingredients = document.querySelector(".ingredientsEditInput")
-    const fieldContainer = document.querySelectorAll(".inputIngredients")
+    const ingredients = document.querySelector(".ingredientsEdit")
+    const fieldContainer = document.querySelector(".ingredientsEditInput")
+    const fieldContainerAll = document.querySelectorAll(".ingredientsEditInput")
 
     // Clone último ingrediente
     const newField = fieldContainer.cloneNode(true)
-    const lengthField = fieldContainer[fieldContainer.length - 1]
+
+    // const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+    const lengthField = fieldContainerAll.length - 1
+    const cloneField = lengthField.cloneNode(true)
     console.log(lengthField)
 
     // if (newField.children[0].value == "") return false
 
-    // newField.children[0].value == ""
+    // newField.children[0].value = ""
     ingredients.appendChild(newField)
+    ingredients.appendChild(cloneField)
 
     console.log("OK")
 }
