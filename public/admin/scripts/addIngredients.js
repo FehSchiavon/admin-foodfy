6d117059134c9e77e9aa3f4c3b2c50221f1e0739
FehSchiavon <felipschiavon@gmail.com>
2020-08-27 06:39:07 -0300

    code review rocketseat

--- public/admin/scripts/addIngredients.js
+++ public/admin/scripts/addIngredients.js
@@ -1,24 +1,32 @@
 // Function ADD newInput
 function addIngredient() {
-    // const fieldContainer = document.createElement("input")
-    // document.body.appendChild(fieldContainer)
 
-    const ingredients = document.querySelector(".ingredientsEdit")
+    const ingredients = document.querySelector(".ingredientsEditInputAll")
     const fieldContainer = document.querySelector(".ingredientsEditInput")
-    const fieldContainerAll = document.querySelectorAll(".ingredientsEditInput")
+
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false
+
+    // Deixa o valor do input vazio
+    newField.children[0].value = ""
+    ingredients.appendChild(newField)
+
 
     // Clone último ingrediente
-    const newField = fieldContainer.cloneNode(true)
+    // const newField = fieldContainer.cloneNode(true)
 
     // const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
-    const lengthField = fieldContainerAll.length - 1
+    // const lengthField = fieldContainerAll.length - 1
     // const cloneField = lengthField.cloneNode(true)
-    console.log(lengthField)
+    // console.log(lengthField)
 
     // if (newField.children[0].value == "") return false
 
     // newField.children[0].value = ""
-    ingredients.appendChild(newField)
+    // ingredients.appendChild(newField)
     // ingredients.appendChild(cloneField)
 
     console.log("OK")
