3ff7911549adb54ba30e71758a2c0605f78586e5
FehSchiavon <felipschiavon@gmail.com>
2020-08-27 20:24:46 -0300

    solver bug addIngredients

--- public/admin/scripts/addIngredients.js
+++ public/admin/scripts/addIngredients.js
@@ -2,7 +2,7 @@
 function addIngredient() {
 
     const ingredients = document.querySelector(".ingredientsEditInputAll")
-    const fieldContainer = document.querySelector(".ingredientsEditInput")
+    const fieldContainer = document.querySelectorAll(".ingredientsEditInput")
 
     // Realiza um clone do último ingrediente adicionado
     const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
@@ -13,21 +13,6 @@ function addIngredient() {
     // Deixa o valor do input vazio
     newField.children[0].value = ""
     ingredients.appendChild(newField)
-
-
-    // Clone último ingrediente
-    // const newField = fieldContainer.cloneNode(true)
-
-    // const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
-    // const lengthField = fieldContainerAll.length - 1
-    // const cloneField = lengthField.cloneNode(true)
-    // console.log(lengthField)
-
-    // if (newField.children[0].value == "") return false
-
-    // newField.children[0].value = ""
-    // ingredients.appendChild(newField)
-    // ingredients.appendChild(cloneField)
 
     console.log("OK")
 }
