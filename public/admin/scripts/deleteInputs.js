c5525e50376df229f53fdcd41f5850dd6a962f47
FehSchiavon <felipschiavon@gmail.com>
2020-09-07 14:09:04 -0300

    linterner all class close ingredients

--- public/admin/scripts/deleteInputs.js
+++ public/admin/scripts/deleteInputs.js
@@ -1,9 +1,27 @@
 // Search
-let closeIngredients = document.getElementsByClassName("closeIngredients")
-let i;
+// let closeIngredients = document.getElementsByClassName("closeIngredients")
+// let i;
 
-for (i = 0; i < closeIngredients.length; i++) {
-    closeIngredients[i].addEventListener("click", function() {
-        this.parentElement.style.display = "none";
+// for (i = 0; i < closeIngredients.length; i++) {
+//     closeIngredients[i].addEventListener("click", function() {
+//         this.parentElement.style.display = "none";
+//     })
+// }
+
+
+// Tentando outra ideia
+
+function deleteInputs(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
+
+let removeInputs = document.getElementsByClassName("closeIngredients")
+console.log(removeInputs.length)
+
+for (i = 0; i < removeInputs.length; i++) {
+    removeInputs[i].addEventListener("click", function() {
+        console.log(i)
     })
 }
