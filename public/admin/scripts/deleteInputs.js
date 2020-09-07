af3a40f3bc5bde554e5cfeb482f06bf8edbb843b
FehSchiavon <felipschiavon@gmail.com>
2020-09-07 08:28:26 -0300

    create addEventLinter

--- public/admin/scripts/deleteInputs.js
+++ public/admin/scripts/deleteInputs.js
@@ -1,2 +1,6 @@
 // Create Function Delete
+function addIngredient() {
+    console.log("OK")
+}
 // Array Search delete
+document.querySelectorAll("#closeIngredients").addEventListener("click", addIngredient)
