50534491961400cb93cb2ae0ac4d6c8162ad499a
FehSchiavon <felipschiavon@gmail.com>
2020-09-10 06:22:03 -0300

    organization function deleteInputs

--- public/admin/scripts/deleteInputs.js
+++ public/admin/scripts/deleteInputs.js
@@ -1,30 +1,32 @@
 // Remove Inputs Ingredients
-let removeInputsIngredients = document.getElementsByClassName("closeIngredients")
-// console.log(removeInputsIngredients.length)
-
 for (let i = 0; i < removeInputsIngredients.length; i++) {
     removeInputsIngredients[i].addEventListener("click", function() {
-
+        
         // Remove mesmo a DIV
         let parentInputsIngredients = this.parentElement
         parentInputsIngredients.parentNode.removeChild(parentInputsIngredients)
-
+        
         // Deixa somente Invisivel
         // this.parentElement.style.display = 'none'
         // console.log(this.parentElement)
-
+        
     })
 }
+
+let removeInputsIngredients = document.getElementsByClassName("closeIngredients")
+// console.log(removeInputsIngredients.length)
+
+
 
 // Remove Inputs Preparation
-let removeInputsPreparation = document.querySelectorAll("#closePreparation")
-console.log(removeInputsPreparation.length)
-
 for (let i = 0; i < removeInputsPreparation.length; i++) {
     removeInputsPreparation[i].addEventListener("click", function() {
-
+        
         let parentInputsPreparation = this.parentElement
         parentInputsPreparation.parentNode.removeChild(parentInputsPreparation)
-
+        
     })
 }
+
+let removeInputsPreparation = document.querySelectorAll("#closePreparation")
+console.log(removeInputsPreparation.length)
